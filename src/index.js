32d483b0761b6c78144c9b90883164c30d1f597b
devindavis5 <devinrdavis555@gmail.com>
2020-10-09 11:54:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const collection = document.getElementById("toy-collection")
-
 //Opens add-toy form
 let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
@@ -11,9 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "block";
       toyFormContainer.addEventListener('submit', function(e) {
         e.preventDefault()
-        // console.log(e.target.name.value)
+        //console.log(e) to find how to grab info you need
+        //e.target[0].value, e.target[1].value
         newToy(e.target)
-        toyFormContainer.remove()
+        // f.reset() //resets info for toy
+        toyFormContainer.remove()  
+        // addToy = !addToy; //allows second toy to be added - changes back to false so that form not showing
     })
     } else {
       toyFormContainer.style.display = "none";
@@ -34,28 +36,27 @@ fetch('http://localhost:3000/toys') //no curly brackets needed on get req
 
 //Add toy to page
 function addAttributes(toy) {
-    const toyDiv = document.createElement('div');
-    toyDiv.class = "card";
+    const toyDiv = document.createElement('div')
+    toyDiv.setAttribute('class', 'card')
     
-    const toyName = document.createElement('h2');
-    toyName.innerHTML = toy.name;
+    const toyName = document.createElement('h2')
+    toyName.innerHTML = toy.name
     
-    const toyImg = document.createElement('img');
-    toyImg.class = "toy-avatar";
-    toyImg.src = toy.image;
+    const toyImg = document.createElement('img')
+    toyImg.setAttribute('class', 'toy-avatar')
+    toyImg.setAttribute('src', toy.image)
 
-    const toyP = document.createElement('p');
-    toyP.innerHTML = `${toy.likes} Likes`;
+    const toyP = document.createElement('p')
+    toyP.innerHTML = `${toy.likes} Likes`
+    toyP.setAttribute('id', toy.id) //needed so that we can refer to id later when adding like
 
     const toyB = document.createElement('button')
-    toyB.class = "like-btn"
-    toyB.id = toy.id //needed so that we can refer to id later when using button
+    toyB.setAttribute('class', 'like-btn')
     toyB.innerHTML = "Like <3"
     toyB.addEventListener('click', function(e){
       e.preventDefault()
-      addLike(e)
+      addLike(e, toyP)
     })
-
     toyDiv.append(toyName, toyImg, toyP, toyB);
     collection.append(toyDiv);
 }
@@ -79,14 +80,13 @@ function newToy(toyData) {
   })
   .then(function(data){
     addAttributes(data)
-
   })
 }
 
 //Add like to database & page
-function addLike(e) {
-  let addedLike = parseInt(e.target.previousElementSibling.innerHTML) + 1
-  fetch(`http://localhost:3000/toys/${e.target.id}`, { //was not working originally because toy id attribute was not given to button 
+function addLike(e, toyP) {
+  const addedLike = parseInt(toyP.innerHTML) + 1
+  fetch(`http://localhost:3000/toys/${toyP.id}`, { //was not working originally because toy id attribute was not given to button 
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
@@ -99,7 +99,7 @@ function addLike(e) {
   .then(function(res) {
     return res.json()
   })
-  .then(function(like) {
-    e.target.previousElementSibling.innerHTML = `${addedLike} Likes`; //making it show up on page
+  .then(function(toy) {
+    toyP.innerText = `${toy.likes} likes`
   })
 }
